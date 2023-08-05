1208937622c3b270279eff20ea54b6f47e0b6ad1
Iskander Karimov <41307855+pronix575@users.noreply.github.com>
2023-08-05 17:06:32 +0300

    Fixes 2

--- src/services/mainLayout/index.ts
+++ src/services/mainLayout/index.ts
@@ -1,3 +1,1 @@
 export { mainLayoutService } from "./mainLayoutService.model";
-export { mainLayoutRoute } from "./mainLayoutService.route";
-export { MainLayoutRoute } from "./mainLayoutService.route.com";
