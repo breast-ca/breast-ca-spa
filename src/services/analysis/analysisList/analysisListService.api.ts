e749b541cf33608240b1d78b5607e114f0fdabd3
Iskander Karimov <41307855+pronix575@users.noreply.github.com>
2024-04-21 19:58:02 +0300

    fix

--- src/services/analysis/analysisList/analysisListService.api.ts
+++ src/services/analysis/analysisList/analysisListService.api.ts
@@ -2,10 +2,12 @@ import { createQuery } from "@farfetched/core";
 import { axios } from "@/api";
 import { AnalysisPagedListResponseDto } from "@/api/shared";
 import { AnalysisListQuery } from "./analysisListService.types";
+import { filterFields } from "@/utils/filterFields";
 
 export const analysisListQuery = createQuery<
   [AnalysisListQuery],
   AnalysisPagedListResponseDto
 >({
-  handler: (params) => axios.get("/analysis", { params }),
+  handler: (params): Promise<AnalysisPagedListResponseDto> =>
+    axios.get("/analysis", { params: filterFields(params) }),
 });
