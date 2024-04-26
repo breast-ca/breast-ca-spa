74506ed2dc83412f4e38a1ccd83ff3141e5f55fd
Iskander Karimov <41307855+pronix575@users.noreply.github.com>
2024-04-26 23:33:06 +0300

    fixes

--- src/components/shared/PatientInfo/PatientInfo.styled.ts
+++ src/components/shared/PatientInfo/PatientInfo.styled.ts
@@ -2,7 +2,16 @@ import { Pen } from "react-bootstrap-icons";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
+export const Content = styled.div`
+  position: relative;
+  width: 100%;
+`;
+
 export const Wrapper = styled.div`
+  width: 368px;
+  right: 16px;
+  top: 16px;
+  position: fixed;
   display: flex;
   flex-direction: column;
   gap: 16px;
--- src/components/shared/PatientInfo/PatientInfo.tsx
+++ src/components/shared/PatientInfo/PatientInfo.tsx
@@ -1,18 +1,20 @@
 import { FC } from "react";
-import { LinkSC, PenSC, Title, Wrapper } from "./PatientInfo.styled";
+import { Content, LinkSC, PenSC, Title, Wrapper } from "./PatientInfo.styled";
 import { Props } from "./PatientInfo.types";
 import { PatientCommonInfo } from "@/services/patients/patientProfile/PatientsProfile/PatientCommonInfo";
 
 export const PatientInfo: FC<Props> = ({ patient, handleEdit }) => {
   return (
-    <Wrapper>
-      <Title>
-        <LinkSC to={`/patients/${patient.id}/common`}>
-          {patient.surname} {patient.name} {patient.middleName || ""}
-        </LinkSC>{" "}
-        {handleEdit && <PenSC onClick={() => handleEdit()} />}
-      </Title>
-      <PatientCommonInfo patient={patient} card={false} />
-    </Wrapper>
+    <Content>
+      <Wrapper>
+        <Title>
+          <LinkSC to={`/patients/${patient.id}/common`}>
+            {patient.surname} {patient.name} {patient.middleName || ""}
+          </LinkSC>{" "}
+          {handleEdit && <PenSC onClick={() => handleEdit()} />}
+        </Title>
+        <PatientCommonInfo patient={patient} card={false} />
+      </Wrapper>
+    </Content>
   );
 };
