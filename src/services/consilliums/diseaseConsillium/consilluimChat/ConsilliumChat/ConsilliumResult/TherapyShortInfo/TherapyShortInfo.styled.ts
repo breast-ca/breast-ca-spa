a28e7d6b8e3ec84dba269b7549361c7d05e4c0ac
Iskander Karimov <41307855+pronix575@users.noreply.github.com>
2024-05-03 22:49:00 +0300

    Show data

--- src/services/consilliums/diseaseConsillium/consilluimChat/ConsilliumChat/ConsilliumResult/TherapyShortInfo/TherapyShortInfo.styled.ts
+++ src/services/consilliums/diseaseConsillium/consilluimChat/ConsilliumChat/ConsilliumResult/TherapyShortInfo/TherapyShortInfo.styled.ts
@@ -3,6 +3,9 @@ import styled from "styled-components";
 
 export const Wrapper = styled(Card)`
   cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
 
 export const Title = styled.div`
--- src/services/consilliums/diseaseConsillium/consilluimChat/ConsilliumChat/ConsilliumResult/TherapyShortInfo/TherapyShortInfo.tsx
+++ src/services/consilliums/diseaseConsillium/consilluimChat/ConsilliumChat/ConsilliumResult/TherapyShortInfo/TherapyShortInfo.tsx
@@ -2,6 +2,11 @@ import { FC } from "react";
 import { Title, TitleWrapper, Wrapper } from "./TherapyShortInfo.styled";
 import { Props } from "./TherapyShortInfo.types";
 import { TherapyStatusBadge } from "@/components/shared/TherapyStatusBadge";
+import {
+  CreatedDate,
+  CreatedDateTitle,
+} from "@/services/analysis/AnalysisCard/AnalysisCard.styled";
+import dayjs from "dayjs";
 
 export const TherapyShortInfo: FC<Props> = ({ therapy }) => {
   return (
@@ -10,6 +15,14 @@ export const TherapyShortInfo: FC<Props> = ({ therapy }) => {
         <Title>{therapy.therapyType}</Title>
         <TherapyStatusBadge status={therapy.therapyStatus} />
       </TitleWrapper>
+      <CreatedDate>
+        <CreatedDateTitle>
+          {therapy.endingTime ? "дата завершения" : "дата начала"}:
+        </CreatedDateTitle>
+        {dayjs(therapy.endingTime || therapy.creationTime).format(
+          "HH:mm DD.MM.YYYY"
+        )}
+      </CreatedDate>
     </Wrapper>
   );
 };
