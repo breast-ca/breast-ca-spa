e274a562e3f4af65b7c94f7195c9b26026ab7818
Iskander Karimov <41307855+pronix575@users.noreply.github.com>
2024-05-04 22:05:23 +0300

    Fixes

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,5 @@
-// export const API_HOST = "http://localhost:8000/api";
-// export const WS_HOST = "http://localhost:8000/";
+export const API_HOST = "http://localhost:8000/api";
+export const WS_HOST = "http://localhost:8000/";
 
-export const API_HOST = "http://192.168.1.166:8000/api";
-export const WS_HOST = "http://192.168.1.166:8000";
+// export const API_HOST = "http://192.168.1.166:8000/api";
+// export const WS_HOST = "http://192.168.1.166:8000";
