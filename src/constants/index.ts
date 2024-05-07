6a222be236018fb1f597e1a55e1df718cb307e42
Iskander Karimov <41307855+pronix575@users.noreply.github.com>
2024-05-07 04:33:11 +0300

    Remove

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,2 @@
 export const API_HOST = "http://89.111.173.139/api";
 export const WS_HOST = "http://89.111.173.139/";
-
-// export const API_HOST = "http://192.168.1.166:8000/api";
-// export const WS_HOST = "http://192.168.1.166:8000";
