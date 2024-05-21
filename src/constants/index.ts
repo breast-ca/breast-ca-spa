aef5b799c19eb698959d1c713d27d8403d4ebba1
Iskander Karimov <41307855+pronix575@users.noreply.github.com>
2024-05-21 22:48:17 +0300

    Update

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,7 +2,7 @@ export const IS_DEV_MODE = import.meta.env.DEV;
 
 export const API_HOST = IS_DEV_MODE
   ? "http://localhost:8000/api"
-  : "https://breast-ca.ru/api";
+  : "https://api.breastca.ru/api";
 export const WS_HOST = IS_DEV_MODE
   ? "http://localhost:8000/"
-  : "https://breast-ca.ru/";
+  : "https://api.breast-ca.ru/";
